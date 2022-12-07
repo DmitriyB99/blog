4363620220c8d4ba4130cd3f5a0524de30a594e5
unknown <dmitry9989@gmail.com>
2022-12-07 21:45:44 +0600

    end back

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -1,5 +1,23 @@
 import PostModel from '../models/Post.js';
 
+export const getLastTags = async (req, res) => {
+  try {
+    const posts = await PostModel.find().limit(5).exec();
+
+    const tags = posts
+      .map((obj) => obj.tags)
+      .flat()
+      .slice(0, 5);
+
+    res.json(tags);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: 'Не удалось получить статьи',
+    });
+  }
+};
+
 export const getAll = async (req, res) => {
   try {
     const posts = await PostModel.find().populate('user').exec();
@@ -43,7 +61,7 @@ export const getOne = async (req, res) => {
 
         res.json(doc);
       },
-    );
+    ).populate('user');
   } catch (error) {
     console.log(error);
     res.status(500).json({
@@ -93,7 +111,7 @@ export const create = async (req, res) => {
       title: req.body.title,
       text: req.body.text,
       imageUrl: req.body.imageUrl,
-      tags: req.body.tags,
+      tags: req.body.tags.split(','),
       user: req.userId,
     });
 
@@ -120,7 +138,7 @@ export const update = async (req, res) => {
         title: req.body.title,
         text: req.body.text,
         imageUrl: req.body.imageUrl,
-        tags: req.body.tags,
+        tags: req.body.tags.split(','),
         user: req.userId,
       },
     );
